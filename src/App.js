bef3c0c0e507fb4d2d551aa3c72e27c6897874c8
Ameya Shrivastava <ameyshri051@gmail.com>
2022-12-28 00:59:56 +0530

    login and register pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme='blue'>Button</Button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/ChatPage.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ChatPage = () => {
+  const [chats, setChats] = useState([]);
+
+  const fetchChats = async () => {
+    const { data } = await axios.get();
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <>
+      <div>ChatPage</div>
+    </>
+  );
+};
+
+export default ChatPage;
