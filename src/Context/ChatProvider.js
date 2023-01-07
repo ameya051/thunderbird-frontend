ad9e69a431dc7ae2ce77cb851aa2639a8518683b
Ameya Shrivastava <75047964+ameya051@users.noreply.github.com>
2023-01-07 23:08:33 +0530

    Update ChatProvider.js

--- src/Context/ChatProvider.js
+++ src/Context/ChatProvider.js
@@ -1,47 +1,23 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const ChatContext = createContext();
+const ChatContext = createContext(null);
 
 const ChatProvider = ({ children }) => {
-  var initState={
-    _id: "",
-    name: "",
-    email:"",
-    pic: "",
-    token: "",
-    
-  }
-  
-  function setLocalStorage(key, value) {
-  try {
-    window.localStorage.setItem("userInfo", JSON.stringify(value));
-  } catch (e) {
-    console.log(e);
-  }
-}
-function getLocalStorage(initialValue) {
-  try {
-    const value = window.localStorage.getItem("userInfo");
-    return value ? JSON.parse(value) : initialValue;
-  } catch (e) {
-    // if error, return initial value
-    return initialValue;
-  }
-}
-  
   const [selectedChat, setSelectedChat] = useState();
   const [user, setUser] = useState();
   const [notification, setNotification] = useState([]);
   const [chats, setChats] = useState([]);
 
   const navigate = useNavigate();
-  
-  
-  
+
   useEffect(() => {
-    getLocalStorage("userInfo", user);
-  }, []);
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [navigate]);
 
   return (
     <ChatContext.Provider
