6035b7532907e090c0005bee7b304bcf8b020ea7
Ameya Shrivastava <75047964+ameya051@users.noreply.github.com>
2023-01-07 23:34:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  
   <BrowserRouter>
-    <ChatProvider>
-      <ChakraProvider>
+    <ChakraProvider>
+      <ChatProvider>
         <App />
-      </ChakraProvider>
-    </ChatProvider>
+      </ChatProvider>
+    </ChakraProvider>
   </BrowserRouter>
+  
 );
